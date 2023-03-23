0da84091d3735cdeac74bf0807eef5aa6980605f
jbprek <jbprek@gmail.com>
2023-03-23 08:50:35 +0000

    WIP

--- udemy-stream-course-v2/code/2-word-count/word-count-docker.sh
+++ udemy-stream-course-v2/code/2-word-count/word-count-docker.sh
@@ -1,10 +1,11 @@
 #!/bash
 
+# WSL Terminal
 # create input topic with two partitions
 kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 2 --topic word-count-input
 
 # create output topic
-kafka-topics.sh --create --bootstrap-server localhost:9092  --replication-factor 1 --partitions 2 --topic word-count-output
+kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 2 --topic word-count-output
 
 # launch a Kafka consumer
 kafka-console-consumer.sh --bootstrap-server localhost:9092 \
@@ -29,3 +30,11 @@ java -jar <your jar here>.jar
 
 # list all topics that we have in Kafka (so we can observe the internal topics)
 kafka-topics.sh --list --bootstrap-server localhost:9092
+
+
+# Clean up
+# create input topic with two partitions
+kafka-topics.sh --delete --bootstrap-server localhost:9092 --topic word-count-input
+
+# create output topic
+kafka-topics.sh --delete --bootstrap-server localhost:9092  --topic word-count-output
